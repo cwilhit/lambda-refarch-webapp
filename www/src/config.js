629e3dfd4ad05cab8c986a8b926a1f40c90ac99e
Craig D Wilhite <craig@upbound.io>
2023-04-12 13:45:39 -0400

    fix cognito URL

--- www/src/config.js
+++ www/src/config.js
@@ -4,7 +4,7 @@
 const config = {
   "aws_user_pools_web_client_id": "5ta17jlg0kvmd4bvepp2q5v6km",     // CognitoClientID
   "api_base_url": "https://00okbsr637.execute-api.us-west-1.amazonaws.com/production",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "stack-up-domain",                   // CognitoDomainName
+  "cognito_hosted_domain": "https://stack-up-domain.auth.us-west-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://main.d10yikfjh2wb5r.amplifyapp.com"                                      // AmplifyURL
 };
 
