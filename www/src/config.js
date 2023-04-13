016c453f24349d87d77c9ee69cdd63677a8425b7
Craig D Wilhite <craig@upbound.io>
2023-04-13 11:43:31 -0400

    Update config.js

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "5ta17jlg0kvmd4bvepp2q5v6km",     // CognitoClientID
-  "api_base_url": "https://00okbsr637.execute-api.us-west-1.amazonaws.com/production",                                     // TodoFunctionApi
+  "aws_user_pools_web_client_id": "5f3la93o87nf9ns9nnhmgp4oh4",     // CognitoClientID
+  "api_base_url": "https://r6yr1eccw5.execute-api.us-west-1.amazonaws.com/production",                                     // TodoFunctionApi
   "cognito_hosted_domain": "stack-up-domain.auth.us-west-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://main.d10yikfjh2wb5r.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://main.d13rl73xsbadjn.amplifyapp.com/"                                      // AmplifyURL
 };
 
 export default config;
